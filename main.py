c70254a75d644f226d8bbdf5870f897918ab6540
Ryan <ryanxaiken@gmail.com>
2024-11-19 21:07:22 -0600

    fixed up the cursor to adjust for character width

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import tkinter as tk
+import tkinter.font as tkFont
+
 
 class Application:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Notor")
+        self.text = ""
 
         self.canvas_manager = CanvasManager(self.root)
         self.cursor_manager = CursorManager(self.canvas_manager)
@@ -15,22 +18,40 @@ class Application:
 
         self.root.mainloop()
     
+    def character_width(event=None,character=None):
+        font = tkFont.Font(font='times')
+        width = font.measure(character)
+
+        print(width)
+
+
+        return width
+
+    
     def on_space_pressed(self, event):
-        self.cursor_manager.cursor_increase()
+        self.text += " "
+        self.cursor_manager.cursor_increase(distance=self.character_width(" "))
         self.cursor_manager.draw_cursor()
     
     def on_backspace_pressed(self, event):
-        self.cursor_manager.cursor_decrease()
+        removed_character = self.text[-1]
+        self.text = self.text[0:-1]
+        self.cursor_manager.draw_character(self.text, backspace=True, width=self.character_width(character=removed_character))
         self.cursor_manager.draw_cursor()
     
     def on_return_pressed(self, event):
+        self.text += "\n"
+        # self.cursor_manager.draw_character(self.text, backspace=False)
         self.cursor_manager.cursor_next_line()
-        self.cursor_manager.draw_cursor()
+        self.cursor_manager.draw_cursor(return_pressed=True)
 
     def on_keydown(self, event):
-        self.cursor_manager.draw_character(event.char)
+        if(event.keycode == 16):
+            return
+        self.text += f"{event.char}"
+        self.cursor_manager.draw_character(self.text,backspace=False,width=self.character_width(event.char))
 
-class CanvasManager:
+class CanvasManager: 
     def __init__(self, root):
         self.canv_x = 400
         self.canv_y = 400
@@ -43,35 +64,41 @@ class CanvasManager:
 class CursorManager:
     def __init__(self, canvas_manager):
         self.canv = canvas_manager.get_canvas()
-        self.cursor_line_height = 20
-        self.cursor_origin = 10
-        self.cursor_max = 385
+        self.cursor_line_height = 14
+        self.cursor_origin = 5.5
+        self.cursor_max = 388
         self.cursor_min = 20
         self.cursor_jump_x = 5
-        self.cursor_jump_y = 18
+        self.cursor_jump_y = 19.1
         self.cursor_x = self.cursor_origin
         self.cursor_y = self.cursor_origin
         
         self.draw_cursor()
 
-    def draw_cursor(self):
+    def draw_cursor(self, return_pressed=None):
         self.canv.delete("cursor")
+        if(return_pressed):
+            self.cursor_x = self.cursor_origin
         self.canv.create_line(self.cursor_x, self.cursor_y, self.cursor_x, self.cursor_y + self.cursor_line_height, tags="cursor")
 
-    def draw_character(self,char):
-        self.canv.create_text(self.cursor_x + self.cursor_origin,self.cursor_y + self.cursor_origin, text=char)
-        self.cursor_increase()
+    def draw_character(self,char, backspace=None, width=None):
+        self.canv.delete("text")
+        self.canv.create_text(self.cursor_origin, self.cursor_origin,anchor= tk.NW, justify=tk.LEFT, width=385, font='times',text=char, tags="text", )
+        if(not backspace):
+            self.cursor_increase(width)
+        else:
+            self.cursor_decrease(width)
         self.draw_cursor()
 
-    def cursor_increase(self):
-        self.cursor_x += self.cursor_jump_x
+    def cursor_increase(self,distance=None):
+        self.cursor_x += int(distance)
         if self.cursor_x > self.cursor_max:
             if self.cursor_y < self.cursor_max:
                 self.cursor_y += self.cursor_jump_y
             self.cursor_x = self.cursor_origin
         
-    def cursor_decrease(self):
-        self.cursor_x -= self.cursor_jump_x
+    def cursor_decrease(self,distance=None):
+        self.cursor_x -= int(distance) 
         if self.cursor_x < self.cursor_min:
             if self.cursor_y > self.cursor_min or self.cursor_y > self.cursor_max:
                 self.cursor_y -= self.cursor_jump_y
